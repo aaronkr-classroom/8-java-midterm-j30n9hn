2a7e06a37a6a07c049a40aebb1fe65e3adb1be89
j30n9hn <50675279+j30n9hn@users.noreply.github.com>
2024-04-23 16:39:06 +0900

    Update LibraryBook.java

--- LibraryBook.java
+++ LibraryBook.java
@@ -6,16 +6,20 @@ public class LibraryBook implements Book {
 	private boolean available;
 
 	public LibraryBook(Book book) {
-		    
-	}
+        this.isbn = book.getIsbn();
+        this.title = book.getTitle();
+        this.author = book.getAuthor();
+        this.year = book.getYear();
+        this.available = book.isAvailable();
+    }
 
-	public LibraryBook(String isbn, String title, String author, int year) {
-		    this.isbn = getIsbn();
-			this.title = getTitle();
-			this.author = getTitle();
-			this.year = getYear();
-			this.available = true;
-	}
+    public LibraryBook(String isbn, String title, String author, int year) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.available = true;
+    }
 
 	@Override
 	public String getIsbn() { return this.isbn; }// return isbn
